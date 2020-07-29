47503734b22119a35aaac309668402739b5ab21e
xuerb <870418012@qq.com>
2020-07-29 17:52:00 +0800

    ajax实现了点击显示详情，可以查看电影详情。 对搜索关键字进行了处理，使用jieba库将它分词，由分词后的每一项查找数据，在经过数据去重后返回。

--- api/views.py
+++ api/views.py
@@ -4,35 +4,67 @@ from .models import AllMovie
 from django.views.generic import View
 import json
 from django.http import JsonResponse
+import jieba
+import time
+
+
 # Create your views here.
 
 
 class AllMovieView(View):
 
     def get(self, request):
+        start_time = time.clock()
         result = {}
         print(request.session['start'])
         if request.is_ajax():
-            title = request.session['title']
-            start = int(request.session['start'])
+            title = request.session['title'].strip()
+            # 先查询包含输入的title
+            movies = list(AllMovie.objects.filter(title__contains=title).order_by('-rate'))
+            # 将输入的title进行分词，使用jieba库的搜索分词模式
+            title_list = jieba.lcut_for_search(title)
             # 查看查询结果有多少
-            count = AllMovie.objects.filter(title__contains=title[0]).count()
+            if len(title) > 1:
+                # 将分词后的结果按照字符长度排序
+                title_list.sort(key=lambda i: len(i), reverse=True)
+                for ele in title_list:
+                    print(ele)
+                    # 如果分词项等于title，说明分词后的结果只有一个，打断循环。
+                    if ele == title:
+                        break
+                    # 如果分词项是常用字，继续下一次循环
+                    if ele in ['的', '了', '呢', '是', '我', '你', '他', ' ']:
+                        continue
+                    movies.extend((AllMovie.objects.filter(title__contains=ele)).order_by('-rate'))
+                    # print(movies)
+                print('去重前:', len(movies))
+                # 查询结果去重，使用set去重会打乱顺序，自己编写
+                temp = []
+                for movie in movies:
+                    if movie not in temp:
+                        temp.append(movie)
+                movies = temp
+                print('去重后:', len(movies))
+            count = len(movies)
             result['count'] = count
-
             start = int(request.session['start'])
-            movies = AllMovie.objects.filter(title__contains=title[0])[start:5+start]
+            # movies = AllMovie.objects.filter(title__contains=title)[start:5+start]
+            # print(len(movies), type(movies), type(movies))
             # 序列化输出json字符串格式数据
-            json_data = serializers.serialize('json', movies, fields=('id', 'title', 'url', 'directors', 'casts',
-                                                                      'rate', 'tags'))
+            return_movies = movies[start: start + 5]
+            json_data = serializers.serialize('json', return_movies, fields=('id', 'title', 'url',
+                                                                             'directors', 'casts', 'rate', 'tags'))
             # 将json字符串转换为python字典
             json_data = json.loads(json_data)
             result['data'] = json_data
-            request.session['start'] = start+5
+            request.session['start'] = start + 5
             result['has_next'] = True if count > start + 5 else False
 
-            print(json_data)
-            res = JsonResponse(result, safe=False, content_type='application/json')
-            print(res, type(res), res.items())
+            # print(json_data)
+            # res = JsonResponse(result, safe=False, content_type='application/json')
+            # print(res, type(res), res.items())
+            end_time = time.clock()
+            print('程序运行时间：{} s'.format(end_time - start_time))
             return JsonResponse(result, safe=False, content_type='application/json')
 
     def post(self, request):
@@ -40,24 +72,26 @@ class AllMovieView(View):
         dicts = request.POST.dict()
         request.session['title'] = dicts.get('title')
         request.session['start'] = 0
-        print(request.body)
-        result = {}
-        print(dicts)
-        movies = AllMovie.objects.filter(title__contains=dicts['title'][0])
-        json_data = serializers.serialize('json', movies, fields=('id', 'title', 'url', 'directors'))
-        json_data = json.loads(json_data)
-        # print(json_data)
-        json_data = json.dumps(json_data, ensure_ascii=False)
-        # print(json_data)
-        result['data'] = json_data
+        # print(request.body)
+        # result = {}
+        # print(dicts)
+        # movies = AllMovie.objects.filter(title__contains=dicts['title'])
+        # json_data = serializers.serialize('json', movies, fields=('id', 'title', 'url', 'directors'))
+        # json_data = json.loads(json_data)
+        # # print(json_data)
+        # json_data = json.dumps(json_data, ensure_ascii=False)
+        # # print(json_data)
+        # result['data'] = json_data
         # return JsonResponse(result, safe=False, content_type='application/json')
-        return render(request, 'api/movie.html', result)
+        return render(request, 'api/movie.html', )
 
 
 def get_intro(request):
-    title = request.GET.get('title')
-    movie = AllMovie.objects.get(title=title)
+    url = request.GET.get('url')
+    li = url.split('/')
+    unique = li[-2]
+    print(unique)
+    movie = AllMovie.objects.get(id=unique)
     intro = movie.intro
     data = {'data': intro}
     return JsonResponse(data, safe=False)
-
